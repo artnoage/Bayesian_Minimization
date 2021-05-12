880a8c43b8a597da976095623ff42c1234cdfe45
artnoage <artnoage@gmail.com>
2021-05-12 11:26:59 +0200

    art

--- Algorithm.py
+++ Algorithm.py
@@ -9,8 +9,10 @@ def OneStep(Archetypes, TransformationFunction, BarycenterPenalty, ArchetypePena
     CovMatrix = CovMatrixInitialization
 
     for i in range(NumberOfIterations):
-        Samples = SampleGeneration(PriorType,MeanMatrix, CovMatrix, SampleSize)
+        Samples = SampleGeneration(PriorType, MeanMatrix, CovMatrix, SampleSize)
         LogLikelihoodValues = LogLikelihood(Samples, (Archetypes, TransformationFunction, BarycenterPenalty, ArchetypePenalty))
+        print("The minimum loglikelihood value is ", np.min(LogLikelihoodValues), "\n")
+        LogLikelihoodValues = 600*LogLikelihoodValues/np.min(LogLikelihoodValues)
         Weights = np.exp(-LogLikelihoodValues)
 
         MeanMatrix = np.array(GaussianReconstruction(PriorType, Samples, Weights)[0])
@@ -19,7 +21,6 @@ def OneStep(Archetypes, TransformationFunction, BarycenterPenalty, ArchetypePena
             factor=np.min(np.diag(CovMatrix))
             CovMatrix=StepSize*CovMatrix/factor
         Barycenter=np.array(Transformation([MeanMatrix[:NumberOfAtoms]],Inputtype="Barycenter", Transformationfunction=TransformationFunction))
-        print("The minimum loglikelihood value is ",np.min(LogLikelihoodValues),"\n")
+
     print("The mean barycenter is  ", Barycenter ,  "\n")
-    #print(BarycenterPenalty,"\n","\n")
     return MeanMatrix, CovMatrix
--- main.py
+++ main.py
@@ -3,8 +3,8 @@ from VisualisationBarycenter import *
 from Algorithm import *
 from Objective import *
 
-NumberOfIterations=500
-SampleSize=100000
+NumberOfIterations=100000
+SampleSize=10000
 
 #Provide the Images
 Archetypes   = np.array([[0.3, 0.3,0.3,0.01,0.01,0.02,0.02,0.02,0.02], [0.02,0.02,0.02,0.02,0.01,0.01,0.3,0.3,0.3]])
@@ -12,21 +12,18 @@ NumberOfAtoms = len(Archetypes[0])
 NumberOfArchetypes = len(Archetypes)
 PlanSize = NumberOfAtoms ** 2
 TotalDimension = NumberOfAtoms + NumberOfArchetypes * PlanSize
-StepSize="Natural"
-
+StepSize= 10**(-7)
 
 
 #Initializing Mean and Covariance
 
-#MeanMatrixInitialization = np.array(np.random.rand(TotalDimension))
-#CovMatrixInitialization = np.identity(TotalDimension)
+MeanMatrixInitialization = np.array(np.random.rand(TotalDimension))
+CovMatrixInitialization = np.identity(TotalDimension)
 
-with open('test.npy', 'rb') as g:
-    MeanMatrixInitialization=np.load(g)
-    CovMatrixInitialization=np.load(g)
-if StepSize != "Natural":
-    factor = np.min(np.diag(CovMatrixInitialization))
-    CovMatrixInitialization = StepSize * CovMatrixInitialization / factor
+#with open('Square.npy', 'rb') as g:
+#    MeanMatrixInitialization=np.load(g)
+#    CovMatrixInitialization=np.load(g)
+
 
 #Itterating Algorithm
 #Transformation is Normalized,Exponential,Normalized Exponential and No Transformation. Penalty types are Square and Entropy.
@@ -40,7 +37,7 @@ MeanMatrix, CovMatrix =OneStep(Archetypes, TransformationFunction="No", Barycent
 # SampleSize=SampleSize, NumberOfIterations=NumberOfIterations,MeanMatrixInitialization=MeanMatrixInitialization,CovMatrixInitialization=CovMatrixInitialization,StepSize=StepSize)
 print(MeanMatrix)
 
-with open('test.npy', 'wb') as f:
+with open('Square.npy', 'wb') as f:
     np.save(f,MeanMatrix)
     np.save(f,CovMatrix)
 
